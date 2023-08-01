8c5d58180b2228c1efaafb3267e8624c0a5fa861
Himanshu Sharma <s.hymanshu@gmail.com>
2023-08-01 13:27:38 +0200

    Create getFilesFromAlien.sh

---
+++ getFilesFromAlien.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#####################
+#  run: $ source getFilesFromAlien.sh inputList.txt AO2D.root
+#  first arg:   a txt file contains the paths from `alien`
+#  second arg:  file you want to download, e.g., AO2D.root or AnalysisResults.root
+#               If you dont specify second arg, it will download everything present in that alien directory.
+######################
+
+inputFileList=$1
+rootFileName=$2
+dirCount=1
+
+if [ $# -eq 0 ]
+  then
+        echo ""
+        echo "Please provide the input file as a First argument!!"
+        echo "Nothing to do..."
+        echo ""
+        return
+fi
+
+for line in $(cat "$inputFileList")
+do
+        mkdir Dir$dirCount
+        cd Dir$dirCount
+        alien_cp alien://$line file:. -name contain_$rootFileName
+        dirCount=$((dirCount+1))
+        cd ../
+done
